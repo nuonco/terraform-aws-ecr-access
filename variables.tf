5c6ba895969b8a399736a903dc405e943c528d55
Jon Morehouse <morehousej09@gmail.com>
2024-03-05 09:02:12 -0600

    fix: allow policy name to be configured

--- variables.tf
+++ variables.tf
@@ -4,6 +4,12 @@ variable "role_name" {
   default = "nuon-ecr-access"
 }
 
+variable "policy_name" {
+  type = string
+  description = "Policy name to create, must be changed if creating more than one role"
+  default = "nuon-ecr-access"
+}
+
 variable "repository_arns" {
   type = list(string)
   description = "Repository ARN to grant access too"
