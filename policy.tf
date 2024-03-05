b5c68c6bcab173b7bf7a2de433da22c720dacfc7
Jon Morehouse <morehousej09@gmail.com>
2024-03-05 08:50:21 -0600

    feat: initial commit

---
+++ policy.tf
@@ -0,0 +1,31 @@
+data "aws_iam_policy_document" "nuon_ecr_access" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:BatchGetImage",
+      "ecr:ListImages",
+      "ecr:ListTagsForResource",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:DescribeImageReplicationStatus",
+      "ecr:DescribeImageScanFindings",
+      "ecr:DescribeImages",
+      "ecr:DescribePullThroughCacheRules",
+      "ecr:DescribeRegistry",
+      "ecr:DescribeRepositories",
+    ]
+    resources = var.repository_arns
+  }
+}
+
+resource "aws_iam_policy" "nuon_ecr_access" {
+  name   = "aws-ecr-access"
+  policy = data.aws_iam_policy_document.nuon_ecr_access.json
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "role_name" {
+  type = string
+  description = "Role name to create, must be changed if creating more than one role"
+  default = "nuon-ecr-access"
+}
+
+variable "repository_arns" {
+  type = list(string)
+  description = "Repository ARN to grant access too"
+}
