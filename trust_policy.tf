ace9162a37f03b7f8ecf91829b0a8735d8a1c385
Jordan Acosta <jordan@nuon.co>
2024-10-17 11:38:23 -0700

    fix: replace old org accounts with new runner accounts (#5)
    To support the V2 runner.

--- trust_policy.tf
+++ trust_policy.tf
@@ -5,15 +5,14 @@ data "aws_iam_policy_document" "nuon_ecr_access_trust" {
       "sts:AssumeRole"
     ]
     principals {
-    type = "AWS"
-    identifiers = [
+      type = "AWS"
+      identifiers = [
         // TODO: we plan on consolidating to a single vendor IAM role for each tenant, meaning this will be only a
         // single account to trust.
         "arn:aws:iam::676549690856:root",
         "arn:aws:iam::007754799877:root",
-        "arn:aws:iam::814326426574:root",
-        "arn:aws:iam::766121324316:root",
-
+        "arn:aws:iam::017820687303:root",
+        "arn:aws:iam::017820687331:root",
       ]
     }
   }
