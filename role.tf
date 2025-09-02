1367a9c393a7373d75f6c8facb1193873fcf771d
Dat Boi Diego <fidiego@users.noreply.github.com>
2025-09-02 10:06:29 -0500

    chore: pin iam-assumable-role (#8)

--- role.tf
+++ role.tf
@@ -1,13 +1,13 @@
 module "nuon_ecr_access" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
-  version = ">= 5.1.0"
+  version = "5.59.0"
 
   create_role = true
 
   role_name = var.role_name
 
-  allow_self_assume_role   = false
-  custom_role_trust_policy = data.aws_iam_policy_document.nuon_ecr_access_trust.json
+  allow_self_assume_role          = false
+  custom_role_trust_policy        = data.aws_iam_policy_document.nuon_ecr_access_trust.json
   create_custom_role_trust_policy = true
   custom_role_policy_arns = [
     aws_iam_policy.nuon_ecr_access.arn
